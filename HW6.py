c7a9321b442f5de448a11336c2bace4e016c2e8a
Erdandrey <58831174+Erdandrey@users.noreply.github.com>
2020-01-15 16:56:01 +0300

    Add files via upload

---
+++ HW6.py
@@ -0,0 +1,8 @@
+Sum = 0
+for one in range(11, 1000000, 2):
+    Reverse = str(one)[::-1]
+    BinOne = bin(one)[2:]
+    BinReverse = BinOne[::-1]
+    if (str(one) == Reverse) and (BinOne == BinReverse):
+        Sum = Sum + one
+print(Sum)
